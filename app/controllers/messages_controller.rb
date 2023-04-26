c102dfdfeebd57e9e347ac5604e0c101914f10ea
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-26 04:26:17 +0000

    complete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,18 +22,10 @@ class MessagesController < ApplicationController
   # POST /messages or /messages.json
   def create
     @message = Message.new(message_params)
-
     @message.user = current_user
+    @message.save
 
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+    SendMessageJob.perform_later(@message)
   end
 
   # PATCH/PUT /messages/1 or /messages/1.json
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import '../stylesheets/app/application.scss'
+import '../stylesheets/application.scss'
 
 Rails.start()
 Turbolinks.start()
